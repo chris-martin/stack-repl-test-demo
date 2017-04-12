1f5ecbd95ba6db1b27e803a9a789c8f91b113af8
Chris Martin <ch.martin@gmail.com>
2017-04-12 16:44:06 -0400

    demo

---
+++ demo/src/Demo.hs
@@ -0,0 +1,8 @@
+module Demo
+    ( someFunc
+    ) where
+
+import Prelude
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
---
+++ demo/test/DemoTest.hs
@@ -0,0 +1,8 @@
+module DemoTest
+    ( someFunc2
+    ) where
+
+import Prelude
+
+someFunc2 :: IO ()
+someFunc2 = putStrLn "someFunc2"
